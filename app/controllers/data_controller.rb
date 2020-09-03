e5b27fa8e0d98d09d0a5c86ef600284e5201788d
m-teraoka-mac <mai.teraoka2525@gmail.com>
2020-09-03 21:55:25 +0900

    React連携サンプルアプリ実装

---
+++ app/controllers/data_controller.rb
@@ -0,0 +1,13 @@
+class DataController < ApplicationController
+  def index
+  end
+
+  def ajax
+    if params[:name]
+      data = Datum.where "name like ?", "%" + params[:name] + "%"
+    else
+      data = Datum.all
+    end
+    render plain: data.to_json
+  end
+end
--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -2,25 +2,37 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Calc = (props) => {
+  let n = props.number;
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total += i;
+  }
+  return (
+    <div>
+      ゼロから{props.number}までの合計は、「{total}」です。
+    </div>
+  );
+};
 
-Hello.defaultProps = {
-  name: 'David'
-}
+Calc.defaultProps = {
+  number: 0
+};
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
+Calc.propTypes = {
+  number: PropTypes.integer
+};
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+document.addEventListener("DOMContentLoaded", () => {
+  let el = (<div>
+    <Calc number="100" />
+    <Calc number="200" />
+    <Calc number="300" />
+  </div>);
+  let dom = document.querySelector('#hello');
+  ReactDOM.render( el, dom );
+});
